6fb710932a239dbfc91509b5c132793ae092d776
Kwon-jun <kjun998@naver.com>
2022-10-31 22:11:48 +0900

    edit legtracker

--- src/omo_r1_leg_tracker.py
+++ src/omo_r1_leg_tracker.py
@@ -3,123 +3,61 @@ import rospy
 
 from leg_tracker.msg import Person, PersonArray, Leg, LegArray
 from geometry_msgs.msg import Twist
+from leg_tracker.srv import Track, TrackResponse
 
-
-# class leg(object):
-
-#     def __init__(self):
-#         rospy.Subscriber("/people_tracked", PersonArray, self.callback)
-
-#     def callback(data):
-#         # rospy.loginfo(data)
-#         n=len(data.people)
-#         x=[]
-#         y=[]
-#         dis=[]
+class leg_tracker():
+    def __init__(self):
+        self.is_active = False
+        self.subs = []
+        self.pubs = {}
+        self.srvs = []
+        self.subs.append(rospy.Subscriber("/people_tracked",PersonArray,self.callback))
+        self.pubs['cmd_vel'] = rospy.Publisher("cmd_vel", Twist,queue_size=1)
+        self.srvs.append(rospy.Service('track', Track, self.handle_track))
         
-#         if cnt[0]==0:
-#             mode=0
-
-#         if mode==0:
-#             for i in range (0,n):
-#                 global id
-#                 x.append(data.people[i].pose.position.x)
-#                 y.append(data.people[i].pose.position.y)
-#                 # rospy.loginfo(x)
-#                 dis.append(pow(pow(x[i],2)+pow(y[i],2),0.5))
-#                 if i==0:
-#                     id=data.people[i].id
-#                 elif i>0:
-#                     if dis[i]<dis[i-1]:
-#                         id=data.people[i].id
-#             mode=1
+    def callback(self, data):
+        # rospy.loginfo(data)
+        if not self.is_active: return
+        n=len(data.people)
+        x=[]
+        y=[]
+        dis=[]
         
-#         elif mode==1:
-#             rospy.loginfo("hi")
-#             for i in range(0,n):
-#                 person=0
-#                 if data.people[i].id==id:
-#                     person=1
-#                     c_x=data.people[i].pose.position.x
-#                     c_y=data.people[i].pose.position.y
-#                     if c_y>=0:
-#                         if c_y<=0.1:
-#                             cmd(0,0)
-#                         else:
-#                             cmd(0, 0.1)
-#                     else:
-#                         if c_y>=-0.1:
-#                             cmd(0,0)
-#                         else:
-#                             cmd(0,-0.1)
-#                 if person==0:
-#                     mode=0
-
-#         cnt[0]+=1
-            
-
-#         rospy.loginfo(id)
-
-#     def cmd(linear, angular):
-#         pub=rospy.Publisher("cmd_vel", Twist, queue_size=10)
-
-#         cmd_msg=Twist()
-#         cmd_msg.linear.x=linear
-#         cmd_msg.linear.y=0
-#         cmd_msg.linear.z=0
-#         cmd_msg.angular.x=0
-#         cmd_msg.angular.y=0
-#         cmd_msg.angular.z=angular
-#         pub.publish(cmd_msg)
-
-def listener():
-    rospy.Subscriber("/people_tracked", PersonArray, callback)
-    rospy.spin()
-    
-def callback(data):
-    # rospy.loginfo(data)
-    n=len(data.people)
-    x=[]
-    y=[]
-    dis=[]
-    
-    for i in range (0,n):
-        global id
-        x.append(data.people[i].pose.position.x)
-        y.append(data.people[i].pose.position.y)
-        # rospy.loginfo(x)
-        dis.append(pow(pow(x[i],2)+pow(y[i],2),0.5))
-        if i==0:
-            id=data.people[i].id
-        elif i>0:
-            if dis[i]<dis[i-1]:
+        for i in range (0,n):
+            global id
+            x.append(data.people[i].pose.position.x)
+            y.append(data.people[i].pose.position.y)
+            # rospy.loginfo(x)
+            dis.append(pow(pow(x[i],2)+pow(y[i],2),0.5))
+            if i==0:
                 id=data.people[i].id
-    
-    for i in range(0,n):
-        if data.people[i].id==id:
-            c_x=data.people[i].pose.position.x
-            c_y=data.people[i].pose.position.y
-            if c_y>=0:
-                if c_y<=0.1:
-                    if c_x<=0.5:
-                        cmd(0,0)
+            elif i>0:
+                if dis[i]<dis[i-1]:
+                    id=data.people[i].id
+        
+        for i in range(0,n):
+            if data.people[i].id==id:
+                c_x=data.people[i].pose.position.x
+                c_y=data.people[i].pose.position.y
+                if c_y>=0:
+                    if c_y<=0.1:
+                        if c_x<=0.5:
+                            self.cmd(0,0)
+                        else:
+                            self.cmd(0.1,0)
                     else:
-                        cmd(0.1,0)
+                        self.cmd(0, 0.1)
                 else:
-                    cmd(0, 0.1)
-            else:
-                if c_y>=-0.1:
-                    if c_x<=0.5:
-                        cmd(0,0)
+                    if c_y>=-0.1:
+                        if c_x<=0.5:
+                            self.cmd(0,0)
+                        else:
+                            self.cmd(0.1,0)
                     else:
-                        cmd(0.1,0)
-                else:
-                    cmd(0,-0.1)                         
-    rospy.loginfo(id)
+                        self.cmd(0,-0.1)                         
+        rospy.loginfo(id)
 
-def cmd(linear, angular):
-        pub=rospy.Publisher("cmd_vel", Twist, queue_size=10)
-
+    def cmd(self, linear, angular):
         cmd_msg=Twist()
         cmd_msg.linear.x=linear
         cmd_msg.linear.y=0
@@ -127,8 +65,20 @@ def cmd(linear, angular):
         cmd_msg.angular.x=0
         cmd_msg.angular.y=0
         cmd_msg.angular.z=angular
-        pub.publish(cmd_msg)
+        self.pubs['cmd_vel'].publish(cmd_msg)
+
+    def handle_track(self, req):
+        if req.str=='stop':
+            rospy.loginfo("stop track")
+            self.is_active = False
+            return TrackResponse('stop')
+        elif req.str=='go':
+            self.is_active = True
+            return TrackResponse('go')
+
 
 if __name__ == '__main__':
     rospy.init_node('leg_controller', anonymous=True)
-    listener()
+    cls_ = leg_tracker()
+    rospy.loginfo("Ready to track.")
+    rospy.spin()
